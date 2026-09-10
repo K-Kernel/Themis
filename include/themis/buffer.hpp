dc13d1a39a2a84ed20951c13b59c86ce4564dc89
KodeVemba <kiamidiogo1@gmail.com>
2026-09-10 22:18:33 +0100

    Buffer struct
     Also add a test for the read_data function

---
+++ include/themis/buffer.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <span>
+#include <vector>
+
+struct Buffer {
+  std::span<const char> buffer_view;
+  std::shared_ptr<void> file_buffer;
+};
+
+Buffer read_data(std::string path) {
+  std::ifstream dataset(path, std::ios_base::binary);
+
+  dataset.seekg(0, std::ios::end);
+  auto dataset_size = dataset.tellg();
+
+  dataset.seekg(0);
+  auto storage = std::make_shared<std::vector<char>>(dataset_size);
+
+  dataset.read(storage->data(), storage->size());
+
+  return Buffer{std::span<const char>(*storage), storage};
+}
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,5 +1,11 @@
 #include <cassert>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
+#include <themis/buffer.hpp>
 
 TEST_CASE("Basic addition") { assert(1 + 1 == 2); }
+TEST_CASE("Load a small file") {
+  Buffer data = read_data("data/test_data1.txt");
+  assert(sizeof(*data.file_buffer) ==
+         sizeof(std::filesystem::file_size("data/test_data1.txt")));
+}
